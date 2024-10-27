3f0183ae9a0d8b10ae87e9f52d4432f78149ffb2
estanivege98 <stanislav@stanPC>
2024-10-27 20:12:57 -0300

    ejercicio 4 implementado

--- Ejercicios/EjerciciosExtra/#1/ej4.sh
+++ Ejercicios/EjerciciosExtra/#1/ej4.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+archivo=/var/log/any-service/error.log
+if [[ $# -eq 0 ]]; then
+    echo "ERROR: no se introdujo un parametro"
+    echo "MODO DE USO: $0 [directorio donde guardar el archivo comprimido]" 
+    exit 1
+elif [[ ! -d $1 ]]; then
+    echo "ERROR: el directorio $1 no existe"
+    exit 1
+fi
+while true; do
+    if [ -f $archivo ]; then
+        if [ $(cat $archivo | grep -c "FATAL ERROR") -gt 0 ]; then
+            $(tar -cvzf $1/user-service.tar.gz /var/log/usr-service)
+            echo $(cat $archivo | grep "FATAL ERROR" | wc -l)
+            exit 2
+        fi
+    fi
+    sleep 180
+done
