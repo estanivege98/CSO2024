976dc3f7f83037b8b1b019ad5bc684317fd6af2b
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-10-27 13:24:29 -0300

    Menu implementado

--- Ejercicios/EjerciciosExtra/ejExamen16.sh
+++ Ejercicios/EjerciciosExtra/ejExamen16.sh
@@ -63,4 +63,60 @@ usuarios(){
 
 # Menu de opciones
 #
+while true; do
+	echo "1. Verificar si un usuario existe"
+	echo "2. Eliminar un usuario"
+	echo "3. Listar los usuarios que contienen un patrón"
+	echo "4. Cantidad de usuarios"
+	echo "5. Listar todos los usuarios"
+	echo "6. Salir"
+	echo "Ingrese una opción: "
+	read opcion
+	case $opcion in
+		1) echo "Ingrese el nombre de usuario: "
+			read usuario
+			existe $usuario
+			if [[ $? -eq 0 ]]; then
+				echo "El usuario $usuario existe"
+			else
+				echo "El usuario $usuario no existe"
+			fi
+		;;
+		2) echo "Ingrese el nombre de usuario: "
+			read usuario
+			eliminar_usuario $usuario
+			if [[ $? -eq 0 ]]; then
+				echo "El usuario $usuario ha sido eliminado"
+			else
+				echo "El usuario $usuario no existe"
+			fi
+		;;
+		3) echo "Ingrese el patrón: "
+			read patron
+			usuarios_con_patron $patron
+			if [[ $? -eq 0 ]]; then
+				echo "Usuarios que contienen el patrón $patron: "
+			else
+				echo "No se encontraron usuarios con el patrón $patron"
+			fi
+		;;
+		4) cantidad
+			if [[ $? -eq 0 ]]; then
+				echo "Cantidad de usuarios: "
+			else
+				echo "No se encontraron usuarios"
+			fi
+		;;
+		5) usuarios
+			if [[ $? -eq 0 ]]; then
+				echo "Usuarios: "
+			else
+				echo "No se encontraron usuarios"
+			fi
+		;;
+		6) break
+		;;
+		*) echo "Opción inválida"
+		;;
+	esac
 
