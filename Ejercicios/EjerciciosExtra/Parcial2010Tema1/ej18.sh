c253c62e2a110e219dd29e7edd9403fc54e1f2d9
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-10-29 19:48:50 -0300

    ejercicio 18 implementado

--- Ejercicios/EjerciciosExtra/Parcial2010Tema1/ej18.sh
+++ Ejercicios/EjerciciosExtra/Parcial2010Tema1/ej18.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+inicializar(){
+    arreglo=()
+}
+agregar_elem(){
+    arreglo+=($1)
+}
+eliminar_elem(){
+    local elemento=$1
+    for i in "${arreglo[@]}"; do
+        if [ "${arreglo[$i]}" -eq $elemento]; then
+            unset arreglo[$i]
+        fi
+    done
+}
+longitud(){
+    echo ${#arreglo[@]}
+}
+imprimir(){
+    for i in "${arreglo[@]}"; do
+        echo $i
+    done
+}
+inicializarConValores(){
+    longitud=$1
+    valorAsignar=$2
+    arreglo=()
+    for ((i=0; i<longitud; i++)); do
+        arreglo+=("$valorAsignar")
+    done
+}
