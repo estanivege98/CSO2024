88f5cce70ab15afbcfbc80102f15570dcf20d8db
estanivege98 <estanislao.vegetti239518@alumnos.info.unlp.edu.ar>
2024-10-31 19:44:54 -0300

    ej18 mejorado

--- Ejercicios/EjerciciosExtra/Parcial2010Tema1/ej18.sh
+++ Ejercicios/EjerciciosExtra/Parcial2010Tema1/ej18.sh
@@ -3,16 +3,22 @@
 inicializar(){
     arreglo=()
 }
+
 agregar_elem(){
     arreglo+=($1)
 }
 eliminar_elem(){
     local elemento=$1
-    for i in "${arreglo[@]}"; do
-        if [ "${arreglo[$i]}" -eq $elemento]; then
+    local elimino=false
+    for i in "${!arreglo[@]}"; do
+        if [[ ${arreglo[$i]} == $elemento ]]; then
             unset arreglo[$i]
+            elimino=true
         fi
     done
+    if [[ $elimino == false ]]; then
+        echo "El elemento no existe en el arreglo"
+    fi
 }
 longitud(){
     echo ${#arreglo[@]}
@@ -30,3 +36,44 @@ inicializarConValores(){
         arreglo+=("$valorAsignar")
     done
 }
+# Menu de opciones
+while true; do
+    echo "1. Inicializar arreglo"
+    echo "2. Agregar elemento"
+    echo "3. Eliminar elemento"
+    echo "4. Longitud del arreglo"
+    echo "5. Imprimir arreglo"
+    echo "6. Inicializar arreglo con valores"
+    echo "7. Salir"
+    read -p "Ingrese una opcion: " opcion
+    case $opcion in
+        1)
+            inicializar
+            ;;
+        2)
+            read -p "Ingrese el elemento a agregar: " elemento
+            agregar_elem $elemento
+            ;;
+        3)
+            read -p "Ingrese el elemento a eliminar: " elemento
+            eliminar_elem $elemento
+            ;;
+        4)
+            longitud
+            ;;
+        5)
+            imprimir
+            ;;
+        6)
+            read -p "Ingrese la longitud del arreglo: " longitud
+            read -p "Ingrese el valor a asignar: " valor
+            inicializarConValores $longitud $valor
+            ;;
+        7)
+            break
+            ;;
+        *)
+            echo "Opcion incorrecta"
+            ;;
+    esac
+done
